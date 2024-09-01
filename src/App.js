903c58c3ddaa8c3f282449ca0d16046d86661a28
BrianMF7 <brianmflores697@gmail.com>
2024-09-01 17:14:14 -0400

    More design/info

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import First from './components/First';
 import VideoBackground from "./components/VideoBackground";
+import Service from './components/Service';
+import "./App.css";
 
 
 
 
 function App() {
   return (
+
+    
     <div className="App">
-     <div>
-      <Navbar />
-      {}
-    </div>
+      <div>
+        <Navbar />
+      </div>
 
-    <div>
-      <First />
-      {}
-    </div>
-    <div>
-      <VideoBackground />
-    </div>
+      <div>
+        <First />
+      </div>
 
-    </div>
+      <div>
+        <VideoBackground />
+      </div>
 
-       
-      
+      <Service />
+
+     
+    </div>
   );
 }
 
